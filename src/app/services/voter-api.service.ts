cf8068e2548226a889215d4a930be71ef4545aa1
poornasekhar <sekharch377@gmail.com>
2020-04-02 14:10:07 +0530

    API URL related changes

--- src/app/services/voter-api.service.ts
+++ src/app/services/voter-api.service.ts
@@ -9,37 +9,39 @@ import { SignUpModel } from '../models/signup.model';
 })
 export class VoterApiService {
 
+  private apiURL='https://localhost:44369/api/';
+
   constructor(private httpclient:HttpClient) { }
 
   validateLogin(loginModel:LoginModel):Observable<any>{
-    return this.httpclient.post('https://localhost:44369/api/login/validateLogin',loginModel);
+    return this.httpclient.post(this.apiURL+'login/validateLogin',loginModel);
   }
 
   validateLoginOTP(loginModel:LoginModel):Observable<any>{
-    return this.httpclient.post('https://localhost:44369/api/login/validateLoginOTP',loginModel);
+    return this.httpclient.post(this.apiURL+'login/validateLoginOTP',loginModel);
   }
 
   submitVote(aadharNumber:any,partyDetailId:any):Observable<any>{
     let urlParams = new URLSearchParams();
     urlParams.append('aadharNumber', aadharNumber);
     urlParams.append('partyDetailId', partyDetailId);
-    return this.httpclient.post('https://localhost:44369/api/login/validateLoginOTP',urlParams);
+    return this.httpclient.post(this.apiURL+'login/validateLoginOTP',urlParams);
   }
 
   validateSignUp(signUpModel:SignUpModel):Observable<any>{
-    return this.httpclient.post('https://localhost:44369/api/signup/validateSignUp',signUpModel);
+    return this.httpclient.post(this.apiURL+'signup/validateSignUp',signUpModel);
   }
 
   validateSignUpOTP(signUpModel:SignUpModel):Observable<any>{
-    return this.httpclient.post('https://localhost:44369/api/signup/validateSignUpOTP',signUpModel);
+    return this.httpclient.post(this.apiURL+'signup/validateSignUpOTP',signUpModel);
   }
 
   registerUser(signUpModel:SignUpModel):Observable<any>{
-    return this.httpclient.post('https://localhost:44369/api/signup/registerUser',signUpModel);
+    return this.httpclient.post(this.apiURL+'signup/registerUser',signUpModel);
   }
 
   getPartyDetails(voterAadharNumber:any):Observable<any>{
-    return this.httpclient.get('https://localhost:44369/api/partydetail/getPartyDetails?aadharNumber='+voterAadharNumber);
+    return this.httpclient.get(this.apiURL+'partydetail/getPartyDetails?aadharNumber='+voterAadharNumber);
   }
 
 }
